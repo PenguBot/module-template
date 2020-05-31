cc681fbeb593c36428340b29ca5e0d0bb065d014
Jacz <itsmejacz@gmail.com>
2020-06-01 09:53:48 +1000

    remove files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { KlasaClient } from "klasa";
-import { BoilerplateClient } from "./lib/Client";
-
-export { BoilerplateClient as Client };
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-module.exports[KlasaClient.plugin] = BoilerplateClient[KlasaClient.plugin];
